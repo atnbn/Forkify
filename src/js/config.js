87f9e9977cca48b4cef5586d7134efd77a044022
atnbn <altan.bnm@gmail.com>
2023-11-20 13:03:44 +0100

    add

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '55334a07-c7bc-49b0-ab04-1469a0d1bc00';
+export const MODAL_CLOSE_SEC = 2.5;
